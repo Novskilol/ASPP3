6c0bae8419f79dcfe2dfde09c49b42f2d9a17a00
johnny bobins <johnny@debianos>
2015-03-27 18:02:18 +0100

    language

--- test.c
+++ test.c
@@ -1,14 +1,6 @@
-struct eltListe{
-  void *elt;
-  struct eltListe *next;
-};
-
-struct pile{
-  top *struct eltListe;
-};
 
 int main(){
   int a=0;
   float b=10;
-  struct pile p;
+  return 0;
 }
