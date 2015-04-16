c0ce0d15f05087bd891c488facf6a3376b31f478
Loris Lucido <loris.lucido@gmail.com>
2015-04-16 14:13:47 +0200

    ajout fichier begin end

--- src/test/rtjhrhu.c
+++ src/test/rtjhrhu.c
@@ -1,4 +1,9 @@
 int main(int argc,char **argv){
-  {int variable; { char test; if (a==b) yolo(); for(int i = 0; i < b; i++) return a;}}{int main(variable,test);}
+  {int variable; {
+char test;
+ if (a==b) yolo();
+ for(int i = 0; i < b; i++)
+ return a;}
+  }{int main(variable,test);}
   return 0;
 }
