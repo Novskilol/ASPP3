00ebe391ff0f2a3bb87eb285ae6aec811b66aa8f
Charles Le Mero <clmero@emi.u-bordeaux1.fr>
2015-04-16 15:12:35 +0200

    exit.c && defun.js

--- src/test/exit.c
+++ src/test/exit.c
@@ -1,11 +1,14 @@
+int max(int a,int b){return (a < b ? b : a);}
+
+
 int main(int argc, char** argc){
 int x=0;
   if (x==0)
-    x=5;
+    x*=10;
   int i=0;
   for (; i<10; i++){
     x++;
-    x++;
+    y++;
   }
   return 0;
 }
