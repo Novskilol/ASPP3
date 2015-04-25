eff4c666c152bc00aad752eba9316a7fe4c3fdd8
Louis Douriez <ldouriez@gmail.com>
2015-04-25 17:37:41 +0200

    Added symboleTable directory, not finished yet

---
+++ src/symboleTable/symboleTable.c
@@ -0,0 +1,24 @@
+#include "symboleTable.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+typedef struct symboleList * SymboleList;
+struct symboleList{
+  struct list
+};
+struct stack_cell{
+  struct stack_cell *next;
+  SymboleList list;
+
+}
+struct symboleTable{
+  struct stack_cell *top;
+};
+
+SymboleTable createSymboleTable()
+{
+  SymboleTable symboleTable=malloc(sizeof(*symboleTable));
+  symboleTable->top=NULL;
+  return symboleTable;
+  
+}
