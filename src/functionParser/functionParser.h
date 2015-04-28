09653accbd11cae09f709a148f6f96a6a578a982
Louis Douriez <ldouriez@gmail.com>
2015-04-28 20:03:27 +0200

    Added rules in functionParser

--- src/functionParser/functionParser.h
+++ src/functionParser/functionParser.h
@@ -1,9 +1,11 @@
 #ifndef FUNCTION_PARSER_H
 #define FUNCTION_PARSER_H
+#include <stdio.h> // Included for FILE *
 typedef struct functionParser * FunctionParser;
-
+typedef void (*FunParserRule) (FILE *,char *);
 FunctionParser createFunctionParser();
 void addStatement(FunctionParser this,char *statementName,char *data);
 void parseFunction(FunctionParser this,char *functionName);
 void resetFunctionParser(FunctionParser this);
+void setRuleForStatement(FunctionParser this,char *statementName,FunParserRule rule);
 #endif
