c7f20dd286166305f8dbac339b7a9768f192c6e6
Louis Douriez <ldouriez@gmail.com>
2015-05-12 11:42:14 +0200

    Modified rapport + added doc for functionParser

--- src/functionParser/functionParser.h
+++ src/functionParser/functionParser.h
@@ -9,6 +9,10 @@ typedef struct functionParser * FunctionParser;
 
 typedef void (*FunParserRule) (FILE *,char *);
 
+/**
+ * @brief Create a function parser with no rules 
+ * @ref{setDefaultRules}
+ */
 FunctionParser createFunctionParser();
 
 void addStatement(FunctionParser this,char *statementName,char *data);
@@ -21,6 +25,13 @@ void destroyFunctionParser(FunctionParser this);
 
 void setRuleForStatement(FunctionParser this,char *statementName,FunParserRule rule);
 
+/**
+ * @label{setDefaultRules}
+ * @brief Set default rules to the function parser 
+ * @ref{defaultReturnRule}
+ * @ref{defaultBriefRule}
+ * @ref{defaultParamRule}
+ */
 void setDefaultRules(FunctionParser this);
 
 void appendBeginDoc();
@@ -40,6 +51,14 @@ void defaultBriefRule(FILE*,char *);
 
 void defaultParamRule(FILE*,char *);
 
+
+/**
+ * @brief create tooltip for the function , if function/variable was documented previously parse it .
+ * @param char *name , the function name or variable name
+ * @param char *returnType  , NULL if its a variable, returntype otherwise
+ * @param int id , the id (class) of the variable/function pointed by name 
+ * @param TableObject to , obtained by searchFunctionSymbolTable
+ */
 void printDocumentation(FunctionParser this, char *name, char *returnType, int id, TableObject to);
 
 #endif
